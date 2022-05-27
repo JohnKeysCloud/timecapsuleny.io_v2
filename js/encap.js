65d80b429c3324f919e2122aa5a8bfd17b105f5e
BL1ZZY <johnblairofficial@gmail.com>
2022-05-27 16:01:30 -0400

    uniform sticky headers and footers, create seperate js files for each page

---
+++ js/encap.js
@@ -0,0 +1,28 @@
+/* 
+  ┌─────────────────────────────────────────────────────────────────────────┐
+  │ opensNavMenuBtnAndDisplaysThemeBtn                     │
+  │ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇                                                    │
+  └─────────────────────────────────────────────────────────────────────────┘
+ */
+
+const menuBtn = document.querySelector('#menu-btn');
+const themeBtn = document.querySelector('#theme-toggle');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+    themeBtn.style.pointerEvents = 'auto';
+    themeBtn.style.overflow = 'visible';
+    themeBtn.style.opacity = '1';
+    themeBtn.style.transform = 'scale(1)'
+    } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+    themeBtn.style.pointerEvents = 'none';
+    themeBtn.style.overflow = 'hidden';
+    themeBtn.style.opacity = '0';
+    themeBtn.style.transform = 'scale(0)'
+  }
+}); 
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* 
   ┌─────────────────────────────────────────────────────────────────────────┐
-  │ opensNavMenuBtnAndDisplaysThemeBtn // homePageOnly                      │
+  │ opensNavMenuBtnAndDisplaysThemeBtn                     │
   │ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇ ⬇                                                    │
   └─────────────────────────────────────────────────────────────────────────┘
  */
