942be99ebc68f01ade4c1983d290aa42cf14dfed
BL1ZZY <johnblairofficial@gmail.com>
2022-06-20 21:10:17 -0400

    clean code some more

--- js/shop.js
+++ js/shop.js
@@ -13,16 +13,8 @@ menuBtn.addEventListener('click', () => {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
-    themeBtn.style.pointerEvents = 'auto';
-    themeBtn.style.overflow = 'visible';
-    themeBtn.style.opacity = '1';
-    themeBtn.style.transform = 'scale(1)'
     } else {
     menuBtn.classList.remove('open');
     menuOpen = false;
-    themeBtn.style.pointerEvents = 'none';
-    themeBtn.style.overflow = 'hidden';
-    themeBtn.style.opacity = '0';
-    themeBtn.style.transform = 'scale(0)'
   }
 }); 
